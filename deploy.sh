8bd2f7cfa18a052f29809cc988e0005102a0190c
Joshua F. Rountree <joshua.rountree@scripps.com>
2018-07-08 17:44:36 -0400

    Fixes

--- deploy.sh
+++ deploy.sh
@@ -35,10 +35,10 @@ git config user.name "OPC CI"
 git config user.email "support@opc.ai"
 
 # If there are no changes (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the spec on this push; exiting."
-    exit 0
-fi
+# if [ -z `git diff --exit-code` ]; then
+#     echo "No changes to the spec on this push; exiting."
+#     exit 0
+# fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
